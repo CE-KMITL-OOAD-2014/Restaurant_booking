d547167ab70fe6d8511d93f01c4f5a76d3aaa559
Tuck Rinlapat <rlt.silverice@gmail.com>
2014-10-15 21:31:08 +0700

    delete ignore

--- app/routes.php
+++ app/routes.php
@@ -26,3 +26,14 @@ Route::post('register_action', function()
         $obj = new RegisterController() ;
         return $obj->store();
 });
+
+Route::get('openRes',function()
+{
+	return View::make('formOpen');
+});
+
+Route::post('openRes_action',function()
+{
+	/*$obj = new RegisterController() ;
+    return $obj->store();*/
+});
